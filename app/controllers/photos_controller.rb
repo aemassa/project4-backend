ecdd500d6c55f42bc70ab1603497a7445925ef21
aemassa <annaemassa@gmail.com>
2015-09-09 14:44:04 -0400

    Adds photos and profiles controllers

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,41 @@
+class PhotosController < ApplicationController
+  #skip_before_action :authenticate, only: :index
+
+  def index
+    render json: Photo.all
+  end
+
+  def show
+    render json: Photo.find(params[:id])
+  end
+
+  def create
+    photo = Photo.create(photo_params)
+    if photo.save
+      render json: photo
+    else
+      render json: photo.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    photo = Photo.find(params[:id])
+    if photo.update(photo_params)
+      photo.save
+      render json: photo
+    else
+      render json: photo.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    photo = Photo.find(params[:id])
+    photo.destroy!
+    head :ok
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:caption)
+  end
+end
---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,42 @@
+class ProfilesController < ApplicationController
+  #skip_before_action :authenticate, only: :index
+
+  def index
+    render json: Profile.all
+    #render json: current_photographer.profile
+  end
+
+  def show
+    render json: Profile.find(params[:id])
+  end
+
+  def create
+    profile = current_photographer.profiles.create(profile_params)
+    if profile.save
+      render json: profile
+    else
+      render json: profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    profile = Profile.find(params[:id])
+    if profule.update(profile_params)
+      profile.save
+      render json: profile
+    else
+      render json: profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    profile = Profile.find(params[:id])
+    profile.destroy!
+    head :ok
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:name, :email, :website, :phone, :city, :state, :zip, :photographer_id)
+  end
+end
