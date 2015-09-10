0fe862aacb70cba6c1c06472c993035133ee5029
aemassa <annaemassa@gmail.com>
2015-09-09 23:20:32 -0400

    Adds profile id to params

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -36,6 +36,6 @@ class PhotosController < ApplicationController
 
   private
   def photo_params
-    params.require(:photo).permit(:caption)
+    params.require(:photo).permit(:caption, :profile_id)
   end
 end
