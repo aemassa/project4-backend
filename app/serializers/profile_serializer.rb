de391f77fee8f3605703f40f8fd18d4aa393ea8a
aemassa <annaemassa@gmail.com>
2015-09-16 15:50:27 -0400

    Adds ID to profile serializer
    Update profile is working!

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,5 +1,5 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :name, :email, :website, :phone, :city, :state, :zip, :image_url
+  attributes :name, :email, :website, :phone, :city, :state, :zip, :image_url, :id
 
   def image_url
     "http://localhost:3000" + object.image.url(:thumb)
