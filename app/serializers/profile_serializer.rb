508980b4385f8f509cfe8f0a6d3dea7138d7f35e
aemassa <annaemassa@gmail.com>
2015-09-22 10:03:21 -0400

    Adds method for medium-sized profile image

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,8 +1,12 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :name, :email, :website, :phone, :city, :state, :zip, :image_url, :id
+  attributes :name, :email, :website, :phone, :city, :state, :zip, :image_url_thumb, :image_url_med, :id
 
-  def image_url
+  def image_url_thumb
     "http://localhost:3000" + object.image.url(:thumb)
+  end
+
+  def image_url_med
+    "http://localhost:3000" + object.image.url(:medium)
   end
 
 end
