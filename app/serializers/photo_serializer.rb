5f898ac044456654adc027daa5ff028eb860fbb2
aemassa <annaemassa@gmail.com>
2015-09-25 23:34:50 -0400

    Updates photo serializer

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,5 +1,5 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :caption, :gallery_image_url_thumb, :gallery_image_url_med, :id
+  attributes :caption, :gallery_image_url_thumb, :gallery_image_url_med, :id, :profile, :photographer
 
   def gallery_image_url_thumb
     "http://localhost:3000" + object.gallery_image.url(:thumb)
@@ -7,6 +7,14 @@ class PhotoSerializer < ActiveModel::Serializer
 
   def gallery_image_url_med
     "http://localhost:3000" + object.gallery_image.url(:medium)
+  end
+
+  def profile
+    object.profile
+  end
+
+  def photographer
+    object.profile.photographer
   end
 
 end
